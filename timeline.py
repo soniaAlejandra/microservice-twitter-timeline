a610cfa5f150bc38af44415c980490452c672239
Luísa Ribeiro <luisarbezerra@hotmail.com>
2018-09-24 23:04:52 -0300

    agora tem validacao

--- timeline.py
+++ timeline.py
@@ -6,8 +6,21 @@ import requests
 
 app = Flask(__name__)
 
+def checaUsuarioExistente(id):
+    try:
+        usuario = requests.get('https://twitter-eng2-users.herokuapp.com/'+id)
+        
+        if usuario.status_code == 404: return 404
+    except Exception as e: return 500
+
 @app.route('/<id>/post', methods=['GET'])
 def post_timeline(id):
+    if checaUsuarioExistente(id) == 404:
+        return jsonify({ 'type': 'NOT_EXISTS', 'description': 'User <%s> does not exist' % id }), 404
+    else:
+        if checaUsuarioExistente(id) == 500:
+            return jsonify({ 'type': 'UNKNOWN', 'description': 'An unknown error was detected'}), 500
+
     r                 = requests.get('http://messages-tpes2.herokuapp.com/')
     mensagens         = r.json()
     mensagens_usuario = []
@@ -20,6 +33,12 @@ def post_timeline(id):
 
 @app.route('/<id>/home', methods=['GET'])
 def home_timeline(id):
+    if checaUsuarioExistente(id) == 404:
+        return jsonify({ 'type': 'NOT_EXISTS', 'description': 'User <%s> does not exist' % id }), 404
+    else:
+        if checaUsuarioExistente(id) == 500:
+            return jsonify({ 'type': 'UNKNOWN', 'description': 'An unknown error was detected'}), 500
+
     r                  = requests.get('http://messages-tpes2.herokuapp.com/')
     mensagens          = r.json()
     r                  = requests.get('https://twitter-eng2-users.herokuapp.com/')
@@ -33,6 +52,11 @@ def home_timeline(id):
     for usuario in range(len(usuarios['users'])):
         if usuarios['users'][usuario]['id'] == int(id):
             for id_amigo in usuarios['users'][usuario]['following']:
+                if checaUsuarioExistente(id) == 404:
+                    return jsonify({ 'type': 'NOT_EXISTS', 'description': 'User <%s> does not exist' % id }), 404
+                else: 
+                    if checaUsuarioExistente(id) == 500:
+                        return jsonify({ 'type': 'UNKNOWN', 'description': 'An unknown error was detected'}), 500
                 for mensagem in range(len(mensagens['messages'])):
                     if mensagens['messages'][mensagem]['user_id'] == int(id_amigo):
                         mensagens_usuarios.append(mensagens['messages'][mensagem]['message'])
