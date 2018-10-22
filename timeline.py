310ac8c45661d5a1129ed03c7a60c50907df0e48
Luísa Ribeiro <luisarbezerra@hotmail.com>
2018-10-22 02:17:37 -0300

    mudando arquitetura da resposta

--- timeline.py
+++ timeline.py
@@ -45,11 +45,10 @@ def home_timeline(id):
     mensagens          = r.json()
     r                  = requests.get('https://twitter-eng2-users.herokuapp.com/')
     usuarios           = r.json()
-    mensagens_usuarios = []
-    
+    mensagens_usuarios = []    
     for mensagem in range(len(mensagens['messages'])):
         if mensagens['messages'][mensagem]['user_id'] == int(id):
-            mensagens_usuarios.append(mensagens['messages'][mensagem]['message'])
+            mensagens_usuarios.append({'mensagem': mensagens['messages'][mensagem]['message'], 'id': id})
     
     for usuario in range(len(usuarios['users'])):
         if usuarios['users'][usuario]['id'] == int(id):
@@ -61,7 +60,7 @@ def home_timeline(id):
                         return jsonify({ 'type': 'UNKNOWN', 'description': 'An unknown error was detected'}), 500
                 for mensagem in range(len(mensagens['messages'])):
                     if mensagens['messages'][mensagem]['user_id'] == int(id_amigo):
-                        mensagens_usuarios.append(mensagens['messages'][mensagem]['message'])
+                        mensagens_usuarios.append({'mensagem': mensagens['messages'][mensagem]['message'], 'id': id_amigo})
     return jsonify({ 'messages': mensagens_usuarios }), 200    
 
 
